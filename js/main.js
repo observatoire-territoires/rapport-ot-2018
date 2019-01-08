d47ad688aeadc5ac2c3011941bb5d8772c72feef
MIROUX Bertrand <BMIROUX@cget.gouv.fr>
2019-01-08 16:56:39 +0100

    Update rapport-ot-2018

--- js/main.js
+++ js/main.js
@@ -1,72 +1,124 @@
 console.log("Etat du document : " + document.readyState);
 
 
+let breakC01Section1 = document.querySelector("#break-c-01-section1");
+let breakC02Section2 = document.querySelector("#break-c-02-section2");
+let breakC03Section3 = document.querySelector("#break-c-03-section3");
+let breakC04Section3 = document.querySelector("#break-c-04-section3");
+let breakC05Section3 = document.querySelector("#break-c-05-section3");
 
-//STICKY
+let scrollNumber = document.querySelectorAll(".scroll-number");
+
+let c01Section1 = document.querySelector("#c-01-section1");
+let c02Section2 = document.querySelector("#c-02-section2");
+let c03Section3 = document.querySelector("#c-03-section3");
+let c04Section3 = document.querySelector("#c-04-section3");
+let c05Section3 = document.querySelector("#c-05-section3");
+let c06Section4 = document.querySelector("#c-06-section4");
+let c07Section4 = document.querySelector("#c-07-section4");
+let c08Section5 = document.querySelector("#c-08-section5");
+let c09Section5 = document.querySelector("#c-09-section5");
+let c10Section6 = document.querySelector("#c-10-section6");
+let c11Section6 = document.querySelector("#c-11-section6");
+
+//initialize the scrollama
+const scroller = scrollama();
+
+function handleStepEnter(response) {
+
+	switch(response.index){
+	case 0:
+		c03Section3.style.display = "block";
+		c04Section3.style.display = "none";
+		c05Section3.style.display = "none";
+		break;
+	case 1:
+		c03Section3.style.display = "none";
+		c04Section3.style.display = "block";
+		c05Section3.style.display = "none";
+		break;
+	case 2:
+		c03Section3.style.display = "none";
+		c04Section3.style.display = "none";
+		c05Section3.style.display = "block";
+		break;
+	case 3:
+		c06Section4.style.display = "block";
+		c07Section4.style.display = "none";
+		break;
+	case 4:
+		c06Section4.style.display = "none";
+		c07Section4.style.display = "block";
+		break;
+	case 5:
+		c08Section5.style.display = "none";
+		c09Section5.style.display = "block";
+		break;
+	case 6:
+		c08Section5.style.display = "block";
+		c09Section5.style.display = "none";
+		break;
+	case 7:
+		c10Section6.style.display = "none";
+		c11Section6.style.display = "block";
+		break;
+	case 8:
+		c10Section6.style.display = "block";
+		c11Section6.style.display = "none";
+		break;
+	}
+}
 
 
+
+
+scroller
+	.setup({
+		container: ".scroll",
+		graphic: ".scroll-graphic",
+		text: ".scroll-text",
+		step: ".break",
+		debug: false,
+		offset: 0.33
+	})
+	.onStepEnter(handleStepEnter);
+
+
+/*
 //SCROLLAMA LAMA
 
+function handleStepEnter(){
+	c01Section1.style.position = "sticky";
+}
 
+function handleStepExit(){
+	c01Section1.style.position = "relative";
+}
 
 
 // instantiate the scrollama
 const scroller = scrollama();
 
 
-function handleStepEnter(response){
-	let elem = document.querySelector("#c-01-section1");
-	elem.style.position = "sticky"
-}
-
-function handleStepExit(response){
-	let elem = document.querySelector("#c-01-section1");
-	elem.style.position = "relative"
-	console.log(elem)
-	console.log(response);
-}
-
 
 // setup the instance, pass callback functions
 scroller
 	.setup({
 		step: "#niv2-c-section1",
-		debug: false,
-		offset:1
+		debug: true,
+		offset: 1
 	})
 	.onStepEnter(handleStepEnter)
 	.onStepExit(handleStepExit);
-
-
-
-
-
-//NEW SKELETON
-/*
-function deplaceElementSmallDevice() {
-
-	//Detect large or small viewport
-	const breakpoint = "(min-width: 1024px)";
-	const largeMedia = window.matchMedia(breakpoint).matches;
-
-	//if largeMedia=true : large device
-	//if largeMedia=false: small device
-
-	if (largeMedia==false){
-
-		console.log("petit écran");
-	}
-
-	if (largeMedia==true){
-	
-		console.log("grand écran");
-	}
-
-}
 */
 
-//OLD SKELETON 
 
+
+//SMALL AND LARGE DEVICE POSITIONNING GRAPHIC
+//-----------------------------------------------------
+//-----------------------------------------------------
+
+/*
 
 function deplaceElementSmallDevice() {
 
@@ -79,26 +131,15 @@ function deplaceElementSmallDevice() {
 	
 
 	//If small device, deplace svg after text, if large device replace svg at the end
-	let aside = document.querySelector("aside");
-	let breakC01Section1 = document.querySelector("#break-c-01-section1");
-	let c01Section1 = document.querySelector("#c-01-section1");
-	let breakC02Section2 = document.querySelector("#break-c-02-section2");
-	let c02Section2 = document.querySelector("#c-02-section2");
-	let breakC03Section3 = document.querySelector("#break-c-03-section3");
-	let c03Section3 = document.querySelector("#c-03-section3");
-	let breakC04Section3 = document.querySelector("#break-c-04-section3");
-	let c04Section3 = document.querySelector("#c-04-section3");
-	let breakC05Section3 = document.querySelector("#break-c-05-section3");
-	let c05Section3 = document.querySelector("#c-05-section3");
 
 
 	if (largeMedia==false){
 
 		breakC01Section1.parentNode.insertBefore(c01Section1,breakC01Section1.nextSibling);
-		breakC02Section2.parentNode.insertBefore(c02Section2,breakC02Section2.nextSibling);
-		breakC03Section3.parentNode.insertBefore(c03Section3,breakC03Section3.nextSibling);
-		breakC04Section3.parentNode.insertBefore(c04Section3,breakC04Section3.nextSibling);
-		breakC05Section3.parentNode.insertBefore(c05Section3,breakC05Section3.nextSibling);
+	//	breakC02Section2.parentNode.insertBefore(c02Section2,breakC02Section2.nextSibling);
+	//	breakC03Section3.parentNode.insertBefore(c03Section3,breakC03Section3.nextSibling);
+	//	breakC04Section3.parentNode.insertBefore(c04Section3,breakC04Section3.nextSibling);
+	//	breakC05Section3.parentNode.insertBefore(c05Section3,breakC05Section3.nextSibling);
 		
 	
 		console.log("petit écran");
@@ -106,12 +147,10 @@ function deplaceElementSmallDevice() {
 
 	if (largeMedia==true){
 
-		aside.append(c01Section1);
-	//	aside.append(c02Section2);
-	//	aside.append(c03Section3);
-	//	aside.append(c04Section3);
-	//	aside.append(c05Section3);
-		
+		scrollNumber[0].append(c01Section1);
+
+
+
 		console.log("grand écran");
 	}
 
@@ -129,3 +168,5 @@ document.onreadystatechange = function(){
 		deplaceElementSmallDevice();
 	}
 };
+
+*/
