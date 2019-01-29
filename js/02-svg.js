5e5b4e1b2247c92bad51679e5958d9ee4803d1f9
Bellice <bellice@tuta.io>
2019-01-29 12:35:25 +0100

    add structure svg 02

---
+++ js/02-svg.js
@@ -0,0 +1,39 @@
+function graph2(){
+
+
+	d3.csv("").then(function(data){
+
+		console.log(data);
+
+
+
+
+
+
+
+	}); //read csv
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+} //fonction graph2
+
+graph2();
