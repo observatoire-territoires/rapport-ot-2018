c4b09497af57aaedb952d99bca69a6b5fd1277bf
Bellice <bellice@tuta.io>
2019-01-30 18:13:26 +0100

    add script svg05

---
+++ js/05-svg.js
@@ -0,0 +1,38 @@
+function graph5(){
+
+
+	d3.csv("").then(function(data){
+
+		console.log(data);
+
+
+
+
+
+
+
+	}); //read csv
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+
+} //fonction graph7
+
+
+graph5();
