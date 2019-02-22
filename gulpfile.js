2d967e96d0f469831360089ab3e5a71ed02438e3
bellice <bellice@tuta.io>
2019-02-22 01:25:00 +0100

    new architecture + improve performance

--- gulpfile.js
+++ gulpfile.js
@@ -2,33 +2,42 @@ let gulp = require("gulp");
 let cleanCSS = require("gulp-clean-css");
 let uglify = require("gulp-uglify");
 let babel = require("gulp-babel");
+let concat = require("gulp-concat");
+let sourcemaps = require("gulp-sourcemaps");
+let rename = require("gulp-rename");
 let pump = require("pump");
  
 
-gulp.task("babel", () =>
-	gulp.src([
-		"src/js/viz/large-device/*.js",
-		"src/js/viz/small-device/*.js"
-])
-		.pipe(babel({
+gulp.task("js", (cb) =>
+	pump([
+		gulp.src([
+			"src/js/nav.js",
+			"src/js/main.js",
+			"src/js/viz/large-device/*.js",
+			"src/js/viz/small-device/*.js"]),
+		sourcemaps.init(),
+		babel({
 			presets: ["@babel/env"]
-		}))
-		.pipe(gulp.dest("test"))
+		}),
+		concat("script.js"),
+		uglify(),
+		rename("script-min.js"),
+		sourcemaps.write("../maps"),
+		gulp.dest("dist/js")
+	],
+	cb
+	)
 );
 
 
-gulp.task("css", () => {
-	return gulp.src("css/*.css")
-		.pipe(cleanCSS({compatibility: "ie8"}))
-		.pipe(gulp.dest("dist"));
-});
-
-gulp.task("compress", function (cb) {
+gulp.task("css", (cb) => 
 	pump([
-		gulp.src("js/viz/large-device/*.js"),
-		uglify(),
-		gulp.dest("dist")
+		gulp.src("src/css/*.css"),
+		sourcemaps.init(),
+		cleanCSS({compatibility: "ie8"}),
+		sourcemaps.write("../maps"),
+		gulp.dest("dist/css")
 	],
 	cb
-	);
-});
+	)
+);
